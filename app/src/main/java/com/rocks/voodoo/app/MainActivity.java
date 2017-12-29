c2326ca80cec68bb621884c971c7e549b313988e
Trubs <fedka@voodoo.rocks>
2017-12-29 14:07:01 +0200

    [+] Maven plugin for the library is added;

--- app/src/main/java/com/rocks/voodoo/app/MainActivity.java
+++ app/src/main/java/com/rocks/voodoo/app/MainActivity.java
@@ -11,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        TestUtil.start(this);
+        TestUti.start(this);
     }
 }
