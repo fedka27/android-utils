d89e2aca7a4af1068e1f994b2cc5b5d3f902c9eb
Trubs <fedka@voodoo.rocks>
2017-12-29 13:41:47 +0200

    [!] Example fixed 3;

--- library/src/main/java/com/rocks/voodoo/library/TestUtils.java
+++
@@ -1,9 +0,0 @@
-package com.rocks.voodoo.library;
-
-import android.content.Context;
-
-public class TestUtils {
-    public static void start(Context context) {
-        // TODO: 29.12.17
-    }
-}
