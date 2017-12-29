7bb4228fd0bc33738603a6c00e36e11c03f8eb3c
Trubs <fedka@voodoo.rocks>
2017-12-29 13:20:35 +0200

    [+] Library added module;

--- app/src/main/java/com/rocks/voodoo/app/MainActivity.java
+++ app/src/main/java/com/rocks/voodoo/app/MainActivity.java
@@ -11,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TestUtils.start(this);
+//        TestUtils.start(this);
     }
 }
